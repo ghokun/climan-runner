7c17b785dbeb99f64068d765efd26c4b2ef3acd4
ghokun <gokhun@gmail.com>
2021-05-16 00:58:44 +0300

    Decrease page size of github client
    Signed-off-by: ghokun <gokhun@gmail.com>

--- pkg/tool/tool.go
+++ pkg/tool/tool.go
@@ -84,7 +84,7 @@ func getLatestTagFromGithub(owner string, repo string, name string, desc string,
 func getReleasesFromGithub(owner string, repo string, name string) (releases []*github.RepositoryRelease, err error) {
 	releases, response, err := client.Repositories.ListReleases(context.Background(), owner, repo, &github.ListOptions{
 		Page:    0,
-		PerPage: 2000,
+		PerPage: 1000,
 	})
 	if err != nil {
 		return releases, err
